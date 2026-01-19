fa699cb37ac12ed764c13dd64e8a8d4af44d7106
Sobinjm <jmsobin@gmail.com>
2026-01-19 17:16:09 +0530

    fix

--- src/components/blocks/navbar/navbar-1/Navbar-1.tsx
+++ src/components/blocks/navbar/navbar-1/Navbar-1.tsx
@@ -93,15 +93,7 @@ export default function NavbarOne({
           </ul>
 
           {/* ============= show contact info in the small device sidebar ============= */}
-          <div className="offcanvas-footer d-lg-none">
-            <div>
-              <NextLink title="info@email.com" className="link-inverse" href="mailto:first.last@email.com" />
-              <br />
-              <NextLink href="tel:0123456789" title="00 (123) 456 78 90" />
-              <br />
-              <SocialLinks />
-            </div>
-          </div>
+
         </div>
       </div>
 
